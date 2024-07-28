e4795c9f0dc41b60f819ca13407b2380c1c9647d
alancavalcante-dev <ala.pereiradocavalcante@gmail.com>
2024-07-28 16:49:45 -0300

    Concertando requisições

--- EstudoMinimalAPI/Program.cs
+++ EstudoMinimalAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<RangoDbContext>(
     options => options.UseSqlite(builder.Configuration.GetConnectionString("RangoDbConStr"))
 );
 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+// builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 
 var app = builder.Build();
@@ -24,24 +24,32 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("rango/", async Task<Results<Ok<List<Rango>>, Ok<Rango>, NoContent>>
     (RangoDbContext db) => {
-        IQueryable<Rango> entity = db.Rangos;
 
-        return TypedResults.Ok(await entity.AsNoTracking().ToListAsync());
+    IQueryable<Rango> entity = db.Rangos;
+    var itemRango = await entity.AsNoTracking().ToListAsync();
+    if (itemRango == null)
+        return TypedResults.NoContent();
+    return TypedResults.Ok(itemRango);
        
-    });
+});
 
+app.MapGet("rango/{id:int}", async Task<Results<Ok<Rango>, NoContent>>
+    (RangoDbContext db, int id) => {
 
+    IQueryable<Rango> entity = db.Rangos;
+    var itemRango = await entity.AsNoTracking().FirstOrDefaultAsync(r => r.Id == id);
+    return TypedResults.Ok(itemRango);
+
+});
 
 
 
 app.MapPost("", async Task<Results<Ok<Rango>, BadRequest<Exception>>>
-    (RangoDbContext db, [FromBody] Rango livro) => {
-        db.Rangos.Add(livro);
+    (RangoDbContext db, [FromBody] Rango rango) => {
+        db.Rangos.Add(rango);
         await db.SaveChangesAsync();
-        return TypedResults.Ok(livro);
-    });
-
-
+        return TypedResults.Ok(rango);
+});
 
 
 
